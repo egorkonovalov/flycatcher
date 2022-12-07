6baf5fdc3c63cd0e357a81093e77fd5d4d151e7f
Egor Konovalov <73017521+egorkonovalov@users.noreply.github.com>
2022-12-07 12:05:14 +0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{njk,md}", "./src/**/*.svg",],
+  theme: {
+    fontFamily: {
+      'display': ['VT323'],
+      'body': ['poppins']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
