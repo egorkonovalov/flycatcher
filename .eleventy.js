b561ba07c87f41c529d71e82737298aeab34d938
Egor Konovalov <73017521+egorkonovalov@users.noreply.github.com>
2023-07-11 11:14:09 +0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets/images");
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
